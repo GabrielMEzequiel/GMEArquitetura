7e48632067a8d3293c0d8e5331b01246a47b4b35
Gabriel Martins <67347136+GabrielMEzequiel@users.noreply.github.com>
2024-04-04 22:52:06 -0300

    Home Adicionada

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {    
+    
+    const open = document.querySelector('.open')
+    const close = document.querySelector('.close')
+    const navbar = document.querySelector('.navbar')
+    const content = document.querySelector('.header-content')
+
+    function openSidebar() {
+        open.classList.add("hidden")
+        close.classList.remove("hidden")
+        navbar.classList.remove("hidden-sm")
+        content.classList.add("hidden-sm")
+    }
+    function closeSidebar() {
+        open.classList.remove("hidden")
+        close.classList.add("hidden")
+        navbar.classList.add("hidden-sm")
+        content.classList.remove("hidden-sm")
+    }
+
+    var textos = ["inspira", "transforma", "inova", "encanta"]
+    var contador = 0
+    const rotative = document.querySelector('.rotative-text')
+
+    function mudarTexto() {
+        rotative.innerHTML = textos[contador]
+        contador++;
+        if (contador >= textos.length) {
+            contador = 0;
+        }
+    }
+
+    setInterval(mudarTexto, 3000);
+});
