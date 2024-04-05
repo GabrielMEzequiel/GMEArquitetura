5b488c69c8f1aa5e24609b005a5a0543e8f98985
Gabriel Martins <67347136+GabrielMEzequiel@users.noreply.github.com>
2024-04-05 12:39:54 -0300

    Projetos Atualizado

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
-const open = document.querySelector('.open')
-const close = document.querySelector('.close')
-const navbar = document.querySelector('.navbar')
-const banner = document.querySelector('.banner-content')
+const open = document.querySelector(".open")
+const close = document.querySelector(".close")
+const navbar = document.querySelector(".navbar")
+const banner = document.querySelector(".banner-content")
 
 function openSidebar() {
         open.classList.add("hidden")
@@ -16,10 +16,52 @@ function closeSidebar() {
         banner.classList.remove("hidden-sm")
 }
 
-document.addEventListener('DOMContentLoaded', function() {  
+const save = document.querySelector(".card-save");
+
+function saveProject() {
+    if (save.classList.contains("saved")) {
+        save.classList.remove("saved");
+    } else {
+        save.classList.add("saved");
+    }
+}
+
+const btnTodos = document.querySelector("#btn-todos");
+const btnComerciais = document.querySelector("#btn-comerciais");
+const btnResidenciais = document.querySelector("#btn-residenciais");
+
+const comerciais = document.querySelector(".comerciais");
+const residenciais = document.querySelector(".residenciais");
+
+function exibirTodos() {
+    comerciais.classList.remove("hidden")
+    residenciais.classList.remove("hidden")
+    btnTodos.classList.add("active")
+    btnComerciais.classList.remove("active")
+    btnResidenciais.classList.remove("active")
+}
+
+function exibirComerciais() {
+    comerciais.classList.remove("hidden")
+    residenciais.classList.add("hidden")
+    btnTodos.classList.remove("active")
+    btnComerciais.classList.add("active")
+    btnResidenciais.classList.remove("active")
+}
+
+function exibirResidenciais() {
+    comerciais.classList.add("hidden")
+    residenciais.classList.remove("hidden")
+    btnTodos.classList.remove("active")
+    btnComerciais.classList.remove("active")
+    btnResidenciais.classList.add("active")
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {  
     var textos = ["inspira", "transforma", "inova", "encanta"]
     var contador = 0
-    const rotative = document.querySelector('.rotative-text')
+    const rotative = document.querySelector(".rotative-text")
 
     function mudarTexto() {
         rotative.innerHTML = textos[contador]
