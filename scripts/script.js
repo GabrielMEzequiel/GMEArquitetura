6731d909c3e89592d4de82aef4c1f7a6bbc7d115
Gabriel Martins <67347136+GabrielMEzequiel@users.noreply.github.com>
2024-06-04 20:24:16 -0300

    Dropdown escolher cores adicionado

--- scripts/script.js
+++ scripts/script.js
@@ -34,11 +34,7 @@ saves.forEach(save => {
 
 function saveProject(event) {
     const save = event.currentTarget
-    if (save.classList.contains("saved")) {
-        save.classList.remove("saved")
-    } else {
-        save.classList.add("saved")
-    }
+    save.classList.toggle("saved");
 }
 
 
@@ -89,90 +85,24 @@ document.addEventListener("DOMContentLoaded", function() {
     setInterval(mudarTexto, 3000);
 });
 
-var x, i, j, l, ll, selElmnt, a, b, c;
-/* Look for any elements with the class "custom-select": */
-x = document.getElementsByClassName("custom-select");
-l = x.length;
-for (i = 0; i < l; i++) {
-  selElmnt = x[i].getElementsByTagName("select")[0];
-  ll = selElmnt.length;
-  /* For each element, create a new DIV that will act as the selected item: */
-  a = document.createElement("DIV");
-  a.setAttribute("class", "select-selected");
-  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-  x[i].appendChild(a);
-  /* For each element, create a new DIV that will contain the option list: */
-  b = document.createElement("DIV");
-  b.setAttribute("class", "select-items select-hide");
-  for (j = 1; j < ll; j++) {
-    /* For each option in the original select element,
-    create a new DIV that will act as an option item: */
-    c = document.createElement("DIV");
-    c.innerHTML = selElmnt.options[j].innerHTML;
-    c.addEventListener("click", function(e) {
-        /* When an item is clicked, update the original select box,
-        and the selected item: */
-        var y, i, k, s, h, sl, yl;
-        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-        sl = s.length;
-        h = this.parentNode.previousSibling;
-        for (i = 0; i < sl; i++) {
-          if (s.options[i].innerHTML == this.innerHTML) {
-            s.selectedIndex = i;
-            h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("same-as-selected");
-            yl = y.length;
-            for (k = 0; k < yl; k++) {
-              y[k].removeAttribute("class");
-            }
-            this.setAttribute("class", "same-as-selected");
-            break;
-          }
-        }
-        h.click();
-    });
-    b.appendChild(c);
-  }
-  x[i].appendChild(b);
-  a.addEventListener("click", function(e) {
-    /* When the select box is clicked, close any other select boxes,
-    and open/close the current select box: */
-    e.stopPropagation();
-    closeAllSelect(this);
-    this.nextSibling.classList.toggle("select-hide");
-    this.classList.toggle("select-arrow-active");
-  });
+function closeAllSelect() {
+    openDropdown(1);
+    openDropdown(2);
 }
 
-function closeAllSelect(elmnt) {
-  /* A function that will close all select boxes in the document,
-  except the current select box: */
-  var x, y, i, xl, yl, arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  xl = x.length;
-  yl = y.length;
-  for (i = 0; i < yl; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i)
+document.addEventListener("click", closeAllSelect());
+
+
+
+function openDropdown(indice) {
+    if (indice == 0) {
+        document.querySelector("#corPredefinida").classList.toggle("show");
+        document.querySelector(".btnPredefinida").classList.toggle("selected");
     } else {
-      y[i].classList.remove("select-arrow-active");
+        document.querySelector("#corPersonalizada").classList.toggle("show");
+        document.querySelector(".btnPersonalizada").classList.toggle("selected");
     }
-  }
-  for (i = 0; i < xl; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
-    }
-  }
-}
-
-document.addEventListener("click", closeAllSelect);
-
-
-
-function openDropdown() {
-    document.querySelector("#dropdown").classList.toggle("show");
-    document.querySelector(".dropbtn").classList.toggle("selected");
+    
 }
 
 let radioButtons = document.querySelectorAll('input[name="colorFormat"]');
@@ -339,8 +269,8 @@ function hslToRgb(h, s, l) {
 updateSliders();
 updateColor();
 
-function changeBackground(backgroundColor = color.value) {
-    openDropdown()
+function changeBackground(indice, backgroundColor = color.value) {
+    openDropdown(indice)
     document.body.style.backgroundColor = backgroundColor;
     document.body.style.color = textColor;
 }
